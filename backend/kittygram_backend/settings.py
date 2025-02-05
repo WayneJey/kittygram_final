3783afaeed67c6d2e015923a6d13374cae61a917
Ivan Boiun <waynejey@yandex.ru>
2025-02-05 21:29:58 +0300

    TRY to watch CATS3

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -108,10 +108,10 @@ USE_TZ = True
 
 
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / 'collected_static'
+STATIC_ROOT = '/static/'
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / '/media/'
+MEDIA_ROOT = '/media/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
