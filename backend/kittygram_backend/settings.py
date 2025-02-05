f5b75122a1f672418086d95b253de8b6c0f1a90f
Ivan Boiun <waynejey@yandex.ru>
2025-02-05 18:54:59 +0300

    Change setting.py

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -6,9 +6,11 @@ from django.core.management.utils import get_random_secret_key
 
 load_dotenv()
 
+BASE_DIR = Path(__file__).resolve().parent.parent
+
 SECRET_KEY = os.getenv('SECRET_KEY', get_random_secret_key())
 
-DEBUG = os.getenv('DEBUG', 'False') == 'True'
+DEBUG = os.getenv('DEBUG', 'false').lower() == 'true'
 
 ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '').split(',')
 
