fa85eaee520e9e0e4806b82c1e458a9d2fc2cfb1
Ivan Boiun <waynejey@yandex.ru>
2025-02-05 18:48:55 +0300

    add env settings

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -1,14 +1,16 @@
 # flake8: noqa
 import os
 from pathlib import Path
+from dotenv import load_dotenv
+from django.core.management.utils import get_random_secret_key
 
-BASE_DIR = Path(__file__).resolve().parent.parent
+load_dotenv()
 
-SECRET_KEY = 'django-insecure-cg6*%6d51ef8f#4!r3*$vmxm4)abgjw8mo!4y-q*uq1!4$-89$'
+SECRET_KEY = os.getenv('SECRET_KEY', get_random_secret_key())
 
-DEBUG = True
+DEBUG = os.getenv('DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = ['127.0.0.1', '123.123.123.123', 'localhost', 'kittygramwayne.zapto.org']
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '').split(',')
 
 INSTALLED_APPS = [
     'django.contrib.admin',
